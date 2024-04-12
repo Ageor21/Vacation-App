c8795d1440f49adc64caf3937b48dda24bf43e7b
Aaron <ageor88@wgu.edu>
2024-04-11 22:15:35 -0400

    B capstone requirements

--- Vacationapp/git/Vacationapp/app/src/main/java/com/example/vacationapp/UI/Excursions.java
+++ Vacationapp/git/Vacationapp/app/src/main/java/com/example/vacationapp/UI/Excursions.java
@@ -11,6 +11,6 @@ public class Excursions extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_excursions);
+        setContentView(R.layout.activity_items_details);
     }
 }
