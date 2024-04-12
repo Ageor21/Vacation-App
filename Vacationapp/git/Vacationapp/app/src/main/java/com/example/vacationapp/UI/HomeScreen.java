b3e242a70c19904a9f48573caaed817415692523
Aaron <ageor88@wgu.edu>
2024-04-12 10:18:04 -0400

    C requirements for capstone project

--- Vacationapp/git/Vacationapp/app/src/main/java/com/example/vacationapp/UI/HomeScreen.java
+++ Vacationapp/git/Vacationapp/app/src/main/java/com/example/vacationapp/UI/HomeScreen.java
@@ -9,17 +9,21 @@ import android.widget.Button;
 
 import com.example.vacationapp.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HomeScreen extends AppCompatActivity {
     public static int numAlert;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        Button button_id=findViewById(R.id.button_id);
+        Button button_id = findViewById(R.id.button_id);
         button_id.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(HomeScreen.this, Vacations.class);
+                Intent intent = new Intent(HomeScreen.this, Vacations.class);
                 startActivity(intent);
             }
         });
